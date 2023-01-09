02b15b705c81ee5ae02a0b550899b1e5cec7753a
man <isugak@yandex.ru>
2023-01-09 12:27:43 +0300

    recipes

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,20 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipes(request, blud):
+    servings = int(request.GET.get("servings", 1))
+    print(servings)
+    context = {
+        'recipe': {
+        }
+    }
+    for ingredient, amount in DATA.items():
+        print(amount)
+        if blud == ingredient:
+            for items in amount:
+                context['recipe'][items] = amount[items] * servings
+                print(amount[items])
+    return render(request, 'calculator/index.html', context)
