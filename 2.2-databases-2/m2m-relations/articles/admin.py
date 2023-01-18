30aac1c50ed0ac7f56b879846c4b79ca7a51d1f3
Илья <w>
2023-01-18 15:34:10 +0300

    m2m

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
 
-from .models import Article
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
 
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        check_list = []
+        for form in self.forms:
+            if len(form.cleaned_data) > 0:
+                check_list.append(form.cleaned_data['is_main'])
+        if check_list.count(True) > 1:
+            raise ValidationError('Основным может быть только один раздел.')
+        if check_list.count(True) == 0:
+            raise ValidationError('Укажите основной раздел.')
+
+        return super().clean()
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 2
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,26 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=15, verbose_name='Тег')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Тег', related_name='scopes', max_length=10)
+    is_main = models.BooleanField(verbose_name='Основной', default=False)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
